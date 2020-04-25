0323823ee4bce818c8d59ae408c6e98197980038
Craig <CraigUkaea@gmail.com>
2020-04-25 16:50:01 +0100

    Added Sin option to calculator

--- examples/calculator.py
+++ examples/calculator.py
@@ -1,6 +1,7 @@
 from PyInquirer import prompt
+import math
 
-operations = [
+questions = [
     {
         'type' : 'list',
         'name' : 'operation',
@@ -15,6 +16,10 @@ operations = [
                 'callback' : lambda answers: print("The answer is: " + str(float(answers['first_value']) - float(answers['second_value']))) # callbacks are not called untill every prompt has been asked
             },
             {
+                'name' : 'Sin',
+                'callback' : lambda answers: print("The answer is: " + str(math.sin(float(answers['first_value'])))) # callbacks are not called untill every prompt has been asked
+            },
+            {
                 'name' : 'Exit',
                 'callback' : lambda x: print("Goodbye!")
             }
@@ -24,7 +29,7 @@ operations = [
         'type' : 'input',
         'name' : 'first_value',
         'message' : 'Enter First Value',
-        'when' : lambda answers : answers['operation'] in ['Add', 'Subtract']
+        'when' : lambda answers : answers['operation'] in ['Add', 'Subtract', 'Sin']
     },
     {
         'type' : 'input',
@@ -34,4 +39,4 @@ operations = [
     }
 ]
 
-answers = prompt(operations)
+answers = prompt(questions)
