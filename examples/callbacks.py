89b616ced52798b3f341da7c8794d51deb9789ff
Craig <CraigUkaea@gmail.com>
2020-04-25 16:39:17 +0100

    updated callbacks example so callbacks accept answers as argument

--- examples/callbacks.py
+++ examples/callbacks.py
@@ -7,15 +7,15 @@ operations = {
     'choices' : [
         {
             'name' : 'Say Hello',
-            'callback' : lambda : [print("Hello!"), prompt(operations)] # print "Hello!", and then start the prompt again
+            'callback' : lambda x: [print("Hello!"), prompt(operations)] # print "Hello!", and then start the prompt again
         },
         {
             'name' : 'Say World',
-            'callback' : lambda : [ print("World!") , prompt(operations)] # print "World!", and then start the prompt again
+            'callback' : lambda x: [ print("World!") , prompt(operations)] # print "World!", and then start the prompt again
         },
         {
             'name' : 'Exit',
-            'callback' : lambda : print("Goodbye!") # print "Goodbye!", but do not start the prompt again
+            'callback' : lambda x: print("Goodbye!") # print "Goodbye!", but do not start the prompt again
         }
     ]
 }
